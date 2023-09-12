9045ab7336808326c776749cdd4edc508ef61c4f
indmillr <indmillr@gmail.com>
2023-09-11 20:25:13 -0500

    TESTING

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,15 +5,13 @@ import Image from "next/image";
 import DarkModeSwitch from "./DarkModeSwitch";
 import Link from "next/link";
 
-import NTALOGO from "/NTALOGO.png";
-
 export default function Logo() {
   return (
     <div className="w-full flex translate-all duration-1000 items-center justify-between h-max-200 pl-3 pr-5 pt-2">
       <div className="flex flex-row -ml-2">
         <Link href="/">
           <Image
-            src={NTALOGO}
+            src="/img/NTALOGO.png"
             width={100}
             height={75}
             alt=""
