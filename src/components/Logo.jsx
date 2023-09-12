a6fdfbcb2972092930cbdcbc480c488696526f62
indmillr <indmillr@gmail.com>
2023-09-11 20:22:58 -0500

    img test

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,17 +5,20 @@ import Image from "next/image";
 import DarkModeSwitch from "./DarkModeSwitch";
 import Link from "next/link";
 
+import NTALOGO from "../../public/NTALOGO.png";
+
 export default function Logo() {
   return (
     <div className="w-full flex translate-all duration-1000 items-center justify-between h-max-200 pl-3 pr-5 pt-2">
       <div className="flex flex-row -ml-2">
         <Link href="/">
           <Image
-            className="cursor-pointer"
-            src="/NTALOGO.png"
+            src={NTALOGO}
             width={100}
             height={75}
             alt=""
+            unoptimized
+            className="cursor-pointer"
           ></Image>
         </Link>
         <div className="-ml-2 mt-1 text-left uppercase font-light  tracking-widest text-xs">
