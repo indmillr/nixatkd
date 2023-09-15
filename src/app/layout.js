62c075e5b275584c8ba77e295f186cc0785bd39b
indmillr <indmillr@gmail.com>
2023-09-15 11:37:00 -0500

    Favicon Update

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,6 +16,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="/img/favicon.ico" />
+      </head>
       <body className={inter.className}>
         <Providers>
           <Logo />
