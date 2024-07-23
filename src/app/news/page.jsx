76594fe0999d46f205ad1a6a4f377d5f53efe1be
Ian Miller <ian.miller@nlsnow.com>
2024-07-23 14:45:14 -0500

    News Page format

--- src/app/news/page.jsx
+++ src/app/news/page.jsx
@@ -1,9 +1,9 @@
 export default function News() {
   return (
-    <div className="w-full h-full flex flex-col translate-all duration-1000 items-center justify-center mt-5 px-5">
+    <div className="w-full min-h-full flex flex-col justify-center pt-[10px] px-5 bg-lighter dark:bg-dark">
       <div className="text-center flex flex-col justify-center xl:text-left h-full mx-auto">
         <h1 className="text-[35px] leading-tight md:text-[60px] md:leading-[1.3] mb-6 font-semibold">
-          NTA <span className="text-red-600 dark:text-blue-600">News</span>.
+          NTA <span className="text-secondary dark:text-primary">News</span>.
         </h1>
 
         <div className="flex flex-col w-full"></div>
